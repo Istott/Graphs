904dfe99276f78735de97867a2aa3ed22b092bd9
isaac stott <isaac.cloyd@gmail.com>
2020-09-09 19:16:14 -0600

    updated ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,9 +1,9 @@
-def earliest_ancestor(ancestors, starting_node, isChild=False):
+def earliest_ancestor(ancestors, starting_node, ancestor = False):
     for parent, child in ancestors:
         if child == starting_node:
             return earliest_ancestor(ancestors, parent, True)
 
-    if isChild:
+    if ancestor == True:
         return starting_node
     else:
         return -1
